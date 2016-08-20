9a3195312e9a0bf7810d85c475d7f752501d73bd
sikorsky228 <slava.sikorsky@gmail.com>
2016-08-21 01:25:18 +0300

    add new packages

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,24 @@ var gulp = require('gulp'),
     rimraf = require('rimraf'),
     browserSync = require("browser-sync"),
     mainBowerFiles = require('main-bower-files'),
-	  reload = browserSync.reload;
-
+	map = require('map-stream'),
+	plumber = require('gulp-plumber'),
+	rigger = require('gulp-rigger'),
+    wait = require('gulp-wait'),
+    cleanCSS = require('gulp-clean-css'),
+    svgSprite = require("gulp-svg-sprites"),
+    filter    = require('gulp-filter'),
+    svg2png   = require('gulp-svg2png'),
+	reload = browserSync.reload;
+ 
+gulp.task('sprites', function () {
+    return gulp.src('./assets/svg/*.svg')
+        .pipe(svgSprite({padding: "5", baseSize: "16"}))
+        .pipe(gulp.dest("./assets")) // Write the sprite-sheet + CSS + Preview 
+        //.pipe(filter("**/*.svg"))  // Filter out everything except the SVG file 
+        .pipe(svg2png())           // Create a PNG 
+        .pipe(gulp.dest("./assets"));
+});
 
 // Move css files to build
 gulp.task('mainfilescss', function() {
@@ -27,29 +43,27 @@ gulp.task('mainfilescss', function() {
 		}}))
         .pipe(gulp.dest('build/css'))
 });
-
-
 var path = {
     build: { //Тут мы укажем куда складывать готовые после сборки файлы
-        html: 'build/',
-        js: 'build/js/',
-        css: 'build/css/',
-        img: 'build/img/',
-        fonts: 'build/fonts/'
+        html: './build/',
+        js: './build/js/',
+        css: './build/css/',
+        img: './build/img/',
+        fonts: './build/fonts/'
     },
     src: { //Пути откуда брать исходники
-        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
-        style: 'src/css/*.scss',
-        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        html: './src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: './src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: './src/css/styles.scss',
+        img: './src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
         fonts: 'src/fonts/**/*.*'
     },
     watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-        html: 'src/**/*.html',
-        js: 'src/js/**/*.js',
-        style: 'src/css/**/*.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        html: './src/**/*.html',
+        js: './src/js/**/*.js',
+        style: './src/css/**/*.scss',
+        img: './src/img/**/*.*',
+        fonts: './src/fonts/**/*.*'
     },
     clean: './build'
 };
@@ -64,6 +78,8 @@ var config = {
     logPrefix: "Frontend_Devil"
 };
 
+
+
 gulp.task('html:build', function () {
     gulp.src(path.src.html) //Выберем файлы по нужному пути
         .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
@@ -72,20 +88,25 @@ gulp.task('html:build', function () {
 
 gulp.task('js:build', function () {
     gulp.src(path.src.js) //Найдем наш main файл
-        .pipe(sourcemaps.init()) //Инициализируем sourcemap
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(sourcemaps.write()) //Пропишем карты
+		.pipe(plumber()) //Проверим ошибки
+		.pipe(rigger())
+        //.pipe(sourcemaps.init()) //Инициализируем sourcemap
+        //.pipe(uglify()) //Сожмем наш js
+        //.pipe(sourcemaps.write()) //Пропишем карты
         .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
         .pipe(reload({stream: true})); //И перезагрузим сервер
 });
 
 gulp.task('style:build', function() {
     gulp.src(path.src.style) //Выберем наш main.scss
-        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(wait(500))
+        //.pipe(sourcemaps.init()) //То же самое что и с js
         .pipe(sass().on('error', sass.logError)) //Скомпилируем
         .pipe(prefixer()) //Добавим вендорные префиксы
-        .pipe(cssmin()) //Сожмем
-        .pipe(sourcemaps.write())
+        //.pipe(cssmin()) //Сожмем
+
+         .pipe(cleanCSS({compatibility: 'ie8'}))
+        //.pipe(sourcemaps.write())
         .pipe(gulp.dest(path.build.css)) //И в build
         .pipe(reload({stream: true}));
 });
